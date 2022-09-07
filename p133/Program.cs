af913b2266046bd2a6327c74f38e627d94c8b418
cavid32 <javidhal@code.edu.az>
2022-09-08 00:29:49 +0400

    first commit

---
+++ p133/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace p133
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Isdifadeci Terefinde Cumle Daxil Olunur.
+            //Daxil Olunan Cumlenin En Uzun Sozunu Tap.
+
+            Console.WriteLine("Cumleni daxil et");
+            string sentence = Console.ReadLine();
+            string[] words = sentence.Split(" ");
+            string BifOfWords = "";
+            int ctr = 0;
+            foreach (String s in words)
+            {
+                if (s.Length > ctr)
+                {
+                    BifOfWords = s;
+                    ctr = s.Length;
+                }
+            }
+            Console.WriteLine(BifOfWords);
+        }
+    }
+}
